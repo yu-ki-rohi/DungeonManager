422ec4b0f699b2affd27f382e236f46bf84e4823
yu-ki-rohi <yukihiro4924@gmail.com>
2024-11-13 14:39:20 +0900

    UIボタンを追加しました

---
+++ Assets/Scripts/InGameScene/UI/UIButton.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButton : MonoBehaviour
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public virtual void PressBehave()
+    {
+
+    }
+
+    public virtual void ReleaseBehave()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
