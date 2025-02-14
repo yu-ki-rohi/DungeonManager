918445035296958af72055671891bb7b4247cb77
yu-ki-rohi <yukihiro4924@gmail.com>
2025-02-14 22:24:00 +0900

    調整を反映しました

--- Assets/Scripts/InGameScene/Characters/CharacterBase.cs
+++ Assets/Scripts/InGameScene/Characters/CharacterBase.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
-using UnityEditor.U2D.Animation;
 using UnityEngine;
 
 public class CharacterBase : MonoBehaviour
